3dcfa2d00bbceaa3e5aee0301137624cb825f186
MeloShen <mellofang1123@gmail.com>
2022-12-10 22:22:04 +0800

    Admissable

---
+++ Code/Admissable/Admissable.java
@@ -0,0 +1,76 @@
+public class Admissable {
+    //input the ordered sequence
+    public static void main(String[] args) {
+        System.out.println("This is the first set:");
+        int[] sequence1 = {5,1,1,1,0};
+        check(sequence1);
+        System.out.println("This is the second set:");
+        int[] sequence2 = {1,1,1,1,1,1,1,0};
+        check(sequence2);
+        System.out.println("This is the third set:");
+        int[] sequence3 = {5,4,2,2,2,2,2,1};
+        check(sequence3);
+
+    }
+    //find the sequence is admissable or not
+    public static void check(int[] sequence) {
+        //print the sequence
+        for (int i : sequence) {System.out.print(i);}
+        System.out.print("\n");
+        //if sequence are all zero vector then output is admissable
+        if (sequence[0] == 0) {
+            for (int i : sequence) {System.out.print(i);}
+            System.out.println("\nHavel and hakiwi: sequence is admissable <=> out put is zero vector");
+            return;
+        }
+        //the sequence does not match the degree
+        if (sequence[0] > sequence.length - 1) {
+            for (int i : sequence) {System.out.print(i);}
+            System.out.println("\nAction stops no simple graph with that degree.");
+            return;
+        }
+        //reduce the sequence
+        sequence = reduce(sequence);
+        //check the sequence dose have negative number
+        if (!checkNegative(sequence)) {
+            for (int i : sequence) {System.out.print(i);}
+            System.out.println("\nAction stops we found to have negative number.");
+            return;
+        }
+        //make the sequence reorded
+        reOrder(sequence);
+        //repeat the function
+        check(sequence);
+    }
+    //this method is to remove the first item in the array and let the rest item reduce 1
+    public static int[] reduce(int[] sequence) {
+        int[] reduceSequence = new int[sequence.length - 1];
+        for (int x = 1; x < sequence.length; x++) {
+            reduceSequence[x - 1] = sequence[x];
+        }
+        for (int y = 0; y < sequence[0]; y++) {
+            reduceSequence[y]--;
+        }
+        return reduceSequence;
+    }
+    //check the sequence have negative number or not
+    public static boolean checkNegative(int[] sequence) {
+        for (int i : sequence) {
+            if (i == -1) {return false;}
+        }
+        return true;
+    }
+    //use the Bubble Sort to reorder the sequence let the sequence form big to small
+    public static int[] reOrder(int[] sequence) {
+        for (int i = 0; i < sequence.length - 1; i++) {
+            for (int j = 0; j < sequence.length - 1 - i; j++) {
+                if (sequence[j + 1] > sequence[j]) {
+                    int temp = sequence[j];
+                    sequence[j] = sequence[j + 1];
+                    sequence[j + 1] = temp;
+                }
+            }
+        }
+        return sequence;
+    }
+}
